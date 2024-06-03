ee5bd09ed4af333628f96e9cb7a37cecffac6cfa
tommasoandina1 <163820513+tommasoandina1@users.noreply.github.com>
2024-06-03 08:42:00 +0200

    Update doosan_h25215.py

--- doosan_h25215.py
+++ doosan_h25215.py
@@ -5,7 +5,7 @@ import casadi as cs
 from math import sqrt
 import matplotlib.pyplot as plt
 
-urdf_path = "/Users/tommasoandina/Doosan_h2515/h2515.blue.urdf"
+urdf_path = "/Users/tommasoandina/Doosan_h2515/model.urdf"
 # The joint list
 joints_name_list = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
 # Specify the root link
@@ -13,9 +13,6 @@ root_link = 'base'
 
 kinDyn = KinDynComputations(urdf_path, joints_name_list, root_link)
 num_dof = kinDyn.NDoF
-
-
-
 
 H = cs.SX.sym('H', 4, 4)
 # The joint values
@@ -60,12 +57,12 @@ class Simulator:
         self.dq = dq
         self.ddq = ddq
 
-    def simulate_q(self, tau, h2):
-        dq = self.simulate_dq(tau, h2)
+    def simulate_q(self, tau, M2, h2):
+        dq = self.simulate_dq(tau, M2, h2)
         self.q += self.dt * dq
         return self.q
 
-    def simulate_dq(self, tau, h2):
+    def simulate_dq(self, tau, M2, h2):
         self.ddq = cs.inv(M2) @ (tau - h2)
         self.dq += self.dt * self.ddq
         return self.dq
@@ -82,19 +79,11 @@ v_b = np.zeros(6)
 s = (np.random.rand(len(joints_name_list)) - 0.5) * 5
 s_dot = (np.random.rand(len(joints_name_list)) - 0.5) * 5
 
-M = kinDyn.mass_matrix_fun()
-M2 = cs.DM(M(H_b, s))
-M2 = M2[6:, 6:]
-
-h = kinDyn.bias_force_fun()
-h2 = cs.DM(h(H_b, s, v_b, s_dot))
-h2 = h2[6:]
-
 q = np.zeros(num_dof)
 
-initial_kp =   500
-kp_increment = 1
-max_kp = 1000
+initial_kp = 1
+kp_increment = 100
+max_kp = 10000
 kd = 2 * sqrt(initial_kp)
 dt = 1.0 / 16.0 * 1e-3
 total_time = 2 * 1e-3
@@ -103,8 +92,6 @@ dq = np.zeros(num_dof)
 ddq = np.zeros(num_dof)
 
 N = int(total_time / dt)
-
-
 
 # plot dati
 time_values = []
@@ -116,9 +103,18 @@ simu = Simulator(q, dt, dq, ddq)
 q_des_np = cs.DM(q_des).full().flatten()
 
 for i in range(N):
+    # Ricalcola M2 e h2 ad ogni iterazione
+    M = kinDyn.mass_matrix_fun()
+    M2 = cs.DM(M(H_b, simu.q))
+    M2 = M2[6:, 6:]
+
+    h = kinDyn.bias_force_fun()
+    h2 = cs.DM(h(H_b, simu.q, v_b, simu.dq))
+    h2 = h2[6:]
+
     tau = ctrl.control(simu.q, simu.dq)
-    simu.simulate_q(tau, h2)
-    simu.simulate_dq(tau, h2)
+    simu.simulate_q(tau, M2, h2)
+    simu.simulate_dq(tau, M2, h2)
     simu.simulate_ddq(M2, tau, h2)
 
     simu_q_np = np.array(simu.q).flatten()
@@ -127,7 +123,7 @@ for i in range(N):
     # Registrare i dati per il plot
     time_values.append(i * dt)
     for j in range(num_dof):
-        q_values[j].append(simu.q[j].full().item())  
+        q_values[j].append(simu.q[j].full().item())
 
     # Aumentare kp
     if ctrl.kp < max_kp:
@@ -136,7 +132,6 @@ for i in range(N):
 
     if np.allclose(simu_q_np, q_des_np, atol=1e-3):
         break
-
 
 # norme
 #def calculate_norm(vector1, vector2):
